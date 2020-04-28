2c8b059c304048a266d46370ebd36bddd2c7e587
zheng-jj <64000939+zheng-jj@users.noreply.github.com>
2020-04-28 20:06:15 +0800

    Used OnClickListener() instead of OnClick

--- MainActivity.java
+++ MainActivity.java
@@ -15,46 +15,50 @@ import java.util.Timer;
 public class MainActivity extends AppCompatActivity {
     private static final String TAG ="Whack-A-Mole!";
     int score = 0;
-    /* Hint
-        - The function setNewMole() uses the Random class to generate a random value ranged from 0 to 2.
-        - Feel free to modify the function to suit your program.
-    */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Log.v(TAG, "Finished Pre-Initialisation!");
+        Button temp = findViewById(R.id.Hole1);
+        Button temp1 = findViewById(R.id.Hole2);
+        Button temp2 = findViewById(R.id.Hole3);
+        final Button[] buttons = new Button[]{temp,temp1,temp2};
+        for(final Button x:buttons)
+        {
+            x.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    setNewMole(v, x, buttons);
+                }
+            });
+        }
     }
-
     protected void onStart(View view){
         super.onStart();
-        setNewMole(view);
         Log.v(TAG, "Starting GUI!");
     }
-
     @SuppressLint("SetTextI18n")
-    public void setNewMole(View view)
+    public void setNewMole(View view, Button chosen, Button[] buttons)
     {
-        int[] buttons = new int[]{R.id.Hole1,R.id.Hole2,R.id.Hole3};
         Random ran = new Random();
         int randomLocation = ran.nextInt(3);
-        Button Randomizedbutton = findViewById(buttons[randomLocation]);
-        Button CurrentClickedButton = findViewById(view.getId());
-        for(int i=0;i<3;i++){
-            Button temp = findViewById(buttons[i]);
+        Button Randomizedbutton = buttons[randomLocation];
+        for(int i=0;i<buttons.length;i++){
+            Button temp = buttons[i];
             String buttonname=temp.getText().toString();
             if(buttonname=="*"){
                 temp.setText("O");
-                if(CurrentClickedButton.getId()==buttons[0]){
+                if(chosen==buttons[0]){
                     Log.v(TAG,"Left Button Clicked!");
                 }
-                else if (CurrentClickedButton.getId()==buttons[1]){
+                else if (chosen==buttons[1]){
                     Log.v(TAG,"Middle Button Clicked!");
                 }
-                else if (CurrentClickedButton.getId()==buttons[2]){
+                else if (chosen==buttons[2]){
                     Log.v(TAG,"Right Button Clicked!");
                 }
-                if(CurrentClickedButton.getId()==temp.getId()){
+                if(chosen==temp){
                     score++;
                     Log.v(TAG,"HIT! SCORE +1!");
                 }
